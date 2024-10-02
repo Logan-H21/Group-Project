4382be27bdec1db452315d08fd7d654db883087d
Logan Fortuin <loganfortuin@MacBook-Pro.local>
2024-10-02 17:09:37 -0500

    changes

---
+++ pord/mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+
+#Create database connection with Django
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'Lorgin2105*',
+)
+
+#middleware
+# cursorObject = dataBase.cursor()
+
+# cursorObject.execute("CREATE DATABASE 3340database")
+print("Hello data base 3340data")
